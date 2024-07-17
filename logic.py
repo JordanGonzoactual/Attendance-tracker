341143dfa4b2f1808450ab41f22c43dfc3d40d0a
Jordan Gonzalez <Jordanaigonzalez@gmail.com>
2024-07-16 20:43:40 -0400

    function to iterate over rows

--- logic.py
+++ logic.py
@@ -5,6 +5,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 # Workbook
 book = openpyxl.load_workbook('D:\\attendance.xlsx')
+ws = book['attendance']
 # staff emails
 staff_mails=['targyarg13@gmail.com', 'largmarg814@gmail.com']
 # Max amount of missed days
@@ -17,13 +18,10 @@ r = sheet.max_row
 no_of_days= []
 # list of students to remind
 l1 =[]
-
-
 # warning messages
 m1 = "Warning!!! you can only miss more day for CI class"
 m2 = " Warning !!! you can only miss one more day for python class"
 m3 = "Warning!!! you can only miss one more day for DM class"
-
 
 # saves excel on every update
 def savefile():
@@ -36,7 +34,12 @@ def check(no_of_days, row_num, b):
     global staff_mails
     global attendance_threshold
     # for students
-    for student in range(0,row_num):
-        if no_of_days == 2:
-            if
+
+# iterates over the rows in excel
+for row in ws.iter_rows(row_num=2,values_only=True,max_row=ws.max_row, min_col=1, max_col=3):
+    student_name = row[0]
+    mailid = row[1]
+    missed_days = row[2]
+
+       
 
