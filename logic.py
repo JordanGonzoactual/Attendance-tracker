4501928d77971528e08d28a98db64a5b43ae6601
Jordan Gonzalez <Jordanaigonzalez@gmail.com>
2024-07-16 20:23:57 -0400

    rearranging logic and variables

--- logic.py
+++ logic.py
@@ -1,16 +1,28 @@
 import openpyxl
 import smtplib
 import email
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 # Workbook
 book = openpyxl.load_workbook('D:\\attendance.xlsx')
 # staff emails
 staff_mails=['targyarg13@gmail.com', 'largmarg814@gmail.com']
-# days limit
-days_limit= 12
 # Max amount of missed days
-attendance_threshold= 13
+attendance_threshold= 3
+# Chooses the sheet
+sheet = book['Sheet1']
+#counting number of rows / student
+r = sheet.max_row
+# number of days students have missed
+no_of_days= []
+# list of students to remind
+l1 =[]
 
 
+# warning messages
+m1 = "Warning!!! you can only miss more day for CI class"
+m2 = " Warning !!! you can only miss one more day for python class"
+m3 = "Warning!!! you can only miss one more day for DM class"
 
 
 # saves excel on every update
@@ -22,5 +34,9 @@ def savefile():
 def check(no_of_days, row_num, b):
     #to use the globally declared lists and strings
     global staff_mails
-    global days_limit
     global attendance_threshold
+    # for students
+    for student in range(0,row_num):
+        if no_of_days == 2:
+            if
+
