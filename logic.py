9130a0bd1891e187e6d630cc203dc7f85803896f
Jordan Gonzalez <Jordanaigonzalez@gmail.com>
2024-07-24 11:34:53 -0400

    Made student class, rearranging code

--- logic.py
+++ logic.py
@@ -15,48 +15,55 @@ sheet = book['Sheet1']
 #counting number of rows / student
 r = sheet.max_row
 # number of days students have missed
-no_of_days= []
+status = no_of_days= []
 # list of students to remind
 l1 =[]
 # warning messages
 m1 = "Warning!!! you can only miss more day for CI class"
 m2 = " Warning !!! you can only miss one more day for python class"
 m3 = "Warning!!! you can only miss one more day for DM class"
+class Student:
+    def __init__(self, name, email, status):
+        self.name= name
+        self.email= email
+        self.status = status 
+       # Marks students late 
+    def mark_late(self):
+        self.status = 'Late'
+    
+    # saves excel on every update
+    def savefile():
+        book.save(r'c:\Users\jorda\OneDrive\Documents\attendance.xlsx')
+        print("saved!")
 
-# saves excel on every update
-def savefile():
-    book.save(r'c:\Users\jorda\OneDrive\Documents\attendance.xlsx')
-    print("saved!")
-
-# To track attendance
-def sendemail(to_address, subject, body):
-    msg = MIMEMultipart()
-    msg['From'] = staff_mail
-    msg['To'] = to_address
-    msg['Subject'] = subject
-    msg.attach(MIMEText(body, 'plain'))
-
-
-
+# To send email
+    def send_mail(to_address, subject, body):
+        msg = MIMEMultipart()
+        msg['From'] = staff_mail
+        msg['To'] = to_address
+        msg['Subject'] = subject
+        msg.attach(MIMEText(body, 'plain'))
+    
+    
     # for students if missed days
-    if missed_days >= attendance_threshold:
-        subject = "Attendance warning"
-        body = (f"Dear {student_name},\n\n"
-                f"You have missed {missed_days} days of class. " 
-                "Please be aware that you are approaching the max days allowed to miss."
-                "Best Regards, \n"
-                "Attendance office")
-        send_email(student_email, subject, body)
-        print(f"Email sent to {student_name} at {student_email}")
+        if no_of_days >= attendance_threshold:
+            subject = "Attendance warning"
+            body = (f"Dear {student_name},\n\n"
+                    f"You have missed {missed_days} days of class. " 
+                    "Please be aware that you are approaching the max days allowed to miss."
+                    "Best Regards, \n"
+                    "Attendance office")
+            send_email(student_email, subject, body)
+            print(f"Email sent to {student_name} at {student_email}")
 
 
-
-
+def Process_attendance(sheet):
+    students= []
 # iterates over the rows in excel
-for row in ws.iter_rows(row_num=2,values_only=True,max_row=ws.max_row, min_col=1, max_col=3):
-    student_name = row[0]
-    mailid = row[1]
-    missed_days = row[2]
+    for row in ws.iter_rows(row_num=2,values_only=True,max_row=ws.max_row, min_col=1, max_col=3):
+        student_name = row[0]
+        mailid = row[1]
+        no_of_days= row[2]
 
        
 
